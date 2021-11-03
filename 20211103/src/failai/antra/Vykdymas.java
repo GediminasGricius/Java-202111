560653a25a2954c80043794ece938c2a7f640e1c
GediminasGricius <g.gricius@ituostas.lt>
2021-11-03 21:19:46 +0200

    Antra uzduotis

--- 20211103/src/failai/antra/Vykdymas.java
+++ 20211103/src/failai/antra/Vykdymas.java
@@ -1,9 +1,27 @@
 package failai.antra;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Vykdymas {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner in=new Scanner(new File("antra.txt"));
+		
+		int n=in.nextInt();
+		
+		int kiekis=0;
+		int buves=in.nextInt();
+		for (int i=0; i<n-1; i++) {
+			int x=in.nextInt();
+			if (buves>x) {
+				kiekis++;
+			}
+			buves=x;
+		}
+		System.out.println("Skaiciu mazesniu uz kaimyna is kaires: "+kiekis);
+		
 
 	}
 
